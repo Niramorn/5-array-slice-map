b9c8b58ac19388b016359f2b6c230257d711f963
Niramorn <57786002+Niramorn@users.noreply.github.com>
2019-12-08 18:59:01 +0700

    Alphabets1

--- Alphabets.go
+++ Alphabets.go
@@ -6,4 +6,15 @@ func main() {
 	alphabets := [2][3]string{{"a","b","c"},{"A","B","c"}}
 	fmt.Println(alphabets)
 	fmt.Println(alphabets[0][1])
-	
+	numbers := [2][3][2]int{
+		{
+			{1, 2},
+			{10, 20},
+			{100, 200},
+		},
+		{
+			{8, 9},
+			{80, 90},
+			{800, 900},
+		},
+	}
